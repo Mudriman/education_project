ffd95fbfeabcef4b41c48b031c8ebcfc1bfb8cef
Mudriman <nmudro@mail.ru>
2023-10-30 08:59:20 +0300

    2

--- Проект/js/main.js
+++ Проект/js/main.js
@@ -1,8 +1,10 @@
 (function () {
-    const header = document.querySelector('.header')
-    windows.onscroll = () => {
-        if (window.pageXOffset > 50) {
-            header.classList.add('header_active')
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.scrollY > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
         }
     }
 }());
